a731133695a3541fc964dd5804737c55ece122a7
Ron Eliav <ron.eliav@post.runi.ac.il>
2023-12-19 17:12:17 +0200

    Homework 1 Done

--- GenThree.java
+++ GenThree.java
@@ -2,8 +2,25 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
+ 
+ 
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int Num1 = Integer.parseInt(args[0]);
+		int Num2 = Integer.parseInt(args[1]);
+		
+		double Random = Math.random();
+		Random = Random * (Num2 - Num1) + Num1;
+		int intRandom1 = (int)Random;
+		Random = Math.random();
+		int intRandom2 = (int)(Random * (Num2 - Num1) + Num1);
+		Random = Math.random();
+		int intRandom3 = (int)(Random * (Num2 - Num1) + Num1);
+		
+		System.out.println(intRandom1);
+		System.out.println(intRandom2);
+		System.out.println(intRandom3);
+		System.out.println("The minimal generated number was " + Math.min(Math.min(intRandom1,intRandom2),intRandom3));
+
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,11 @@
  * Write a program that tests if three given integers form a triangle.
  */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
+	public static void main(String[] args){
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		int num3 = Integer.parseInt(args[2]);
+		boolean checkIfTriangle = (num1 + num2) > num3 && (num2 + num3) > num1 && (num1 + num3) > num2;
+		System.out.println(num1 + ", " + num2 + ", " + num3 + ": " + checkIfTriangle);
 	}
 }
